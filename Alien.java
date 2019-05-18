b90580f746b62c7efa5a74f5e992a5ebc91ef8c0
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-18 14:08:54 -0400

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -1,1 +1,32 @@
+package CollisionDetection;
 
+public class Alien extends Sprite {
+  
+  private final int INITIAL_X = 400;
+  
+  public Alien(int x, int y) {
+    super(x, y);
+    
+    initAlien();
+	}
+	
+	private void initAlien() {
+		
+		loadImage("src/resources/Alien.png");
+		getImageDimensions();
+	}
+	
+	public void move() {
+		
+		if (x < 0) {
+			x = INITIAL_X;
+		}
+		
+		x -= 1;
+		
+		//Aliens return to the screen on the right side after they disappeared on the left//
+	}
+  }
+	
+	
+  
